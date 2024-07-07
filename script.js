09ca1c130fe8b81aad69e6c92ec82760094c31f1
Dery Jon <yusupoviskandar1005@gmail.com>
2024-07-08 01:26:49 +0500

    feat: add skills section (2024-07-08, 1:26)

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuIcon = document.getElementById('menu-icon');
-    const navList = document.getElementById('menu_list');
-  
-    menuIcon.addEventListener('click', function() {
-      navList.classList.toggle('active');
-      menuIcon.classList.toggle('active');
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.getElementById("menu-icon");
+  const navList = document.getElementById("menu_list");
+  const links = document.querySelectorAll(".menu_list .nav_links");
+
+  [...links].forEach((link) => {
+    link.addEventListener("click", toggleMenu);
   });
-  
+
+  function toggleMenu() {
+    navList.classList.toggle("active");
+    menuIcon.classList.toggle("active");
+  }
+  menuIcon.addEventListener("click", toggleMenu);
+});
